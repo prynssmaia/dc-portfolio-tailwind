4c9889098345fe575821ef617d3c489c9f720791
Prynss <prynss_lp@hotmail.com>
2022-05-06 22:19:19 -0300

    project setup

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'body': '#17171F',
+        'selected-text': '#87021B',
+        'theme': '#E10234',
+        'nav': '#404053',
+        'secondary': '#9191A4',
+        'badge': '#3F3F51',
+        'input-border': '#565666',
+        'input': '#2A2A35'
+      },
+      fontFamily: {
+        'poppins': ["'Poppins'", 'sans-serif']
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  }
+}
